29368deefdc08cd98ec21d39e19596ed510648cc
geoff <geoff.griffiths@vizlib.com>
2022-12-17 10:37:49 +0000

    set up basic gorm db connection

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+)
+
+var dsn = "root:password@tcp(localhost:3306)/git?charset=utf8mb4"
+var dbConnection, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+type DbFields struct {
+	ID   int
+	Name string
+	Year string
+}
 
 func main() {
-	fmt.Println("in git now")
+	http.HandleFunc("/", DatabaseCreate)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func DatabaseCreate(w http.ResponseWriter, r *http.Request) {
+	dbFields := DbFields{
+		ID:   3,
+		Name: "Geoff",
+		Year: "1965",
+	}
+
+	if err := dbConnection.Create(&dbFields).Error; err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.NewEncoder(w).Encode(dbFields); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Fields added", dbFields)
+
 }
